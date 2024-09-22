7628d8cc6c4a42ed43693f00cbaf96d5a47d1092
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-22 12:28:51 -0400

    prototype model

--- sandbox.js
+++ sandbox.js
@@ -3,48 +3,24 @@ class User{
     // set up properties
     this.username = username;
     this.email = email;
-    this.score = 0;
-  }
-
-  login(){
-    console.log(`${this.username} has logged in.`);
-    return this;
-  }
-
-  logout(){
-    console.log(`${this.username} has logged out.`);
-    return this;
-  }
-
-  incScore(){
-    this.score++;
-    console.log(`${this.username}'s score is now ${this.score}.`);
-    return this;
   }
 }
 
-class Admin extends User {
-  constructor(username, email, password, title){
-    super(username, email);
-    this.password = password;
-    this.title = title;
-  }
-  deleteUser(user){
-    users = users.filter(u => {
-      return u.username !== user.username;
-    });
-  }
+User.prototype.login = function(){
+  console.log(`${this.username} has logged in.`);
 }
 
-// create instances of User and Admin class and test methods
+User.prototype.logout = function(){
+  console.log(`${this.username} has logged out.`);
+}
+
+// create instances of User class and test methods
 
 const userOne = new User('mario', 'mario@gmail.com');
 const userTwo = new User('luigi', 'luigi@gmail.com');
-const adminOne = new Admin('admin', 'admin@gmail.com', 'admin123', 'server admin');
 
-let users = [userOne, userTwo, adminOne];
-console.log(users);
+console.log(userOne, userTwo);
 
-adminOne.deleteUser(userTwo);
-console.log(users);
+userOne.login();
+userTwo.logout();
 

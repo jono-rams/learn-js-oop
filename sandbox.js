644c1f84f12475df40926cfc5a874c91212848c3
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-22 12:35:27 -0400

    prototypal inheritance model

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,37 @@
-class User{
-  constructor(username, email){
-    // set up properties
-    this.username = username;
-    this.email = email;
-  }
-}
+function User(username, email){
+  // set up properties
+  this.username = username;
+  this.email = email;
+};
 
 User.prototype.login = function(){
   console.log(`${this.username} has logged in.`);
+  return this;
 }
 
 User.prototype.logout = function(){
   console.log(`${this.username} has logged out.`);
+  return this;
+}
+
+function Admin(username, email, title){
+  User.call(this, username, email);
+  this.title = title;
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function(){
+  // delete a user
 }
 
 // create instances of User class and test methods
 
 const userOne = new User('mario', 'mario@gmail.com');
 const userTwo = new User('luigi', 'luigi@gmail.com');
+const adminOne = new Admin('shaun', 'shaun@gmail.com', 'system admin');
 
-console.log(userOne, userTwo);
+console.log(userOne, userTwo, adminOne);
 
-userOne.login();
-userTwo.logout();
+userOne.login().logout();
 
